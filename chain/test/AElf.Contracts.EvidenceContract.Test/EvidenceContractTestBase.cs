34f139465f244fa2c705ed438327525a1a533522
huangdongmei <849013525@qq.com>
2020-04-08 18:06:28 +0800

    完成test模块的编写

---
+++ chain/test/AElf.Contracts.EvidenceContract.Test/EvidenceContractTestBase.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Linq;
+using Acs0;
+using AElf.Contracts.TestKit;
+using AElf.Cryptography.ECDSA;
+using AElf.Kernel;
+using AElf.Types;
+using Google.Protobuf;
+using Volo.Abp.Threading;
+
+namespace AElf.Contracts.EvidenceContract
+{
+    public class EvidenceContractTestBase : ContractTestBase<EvidenceContractTestModule>
+    {
+        internal EvidenceContractContainer.EvidenceContractStub EvidenceContractStub { get; set; }
+        private ACS0Container.ACS0Stub ZeroContractStub { get; set; }
+
+        private Address EvidenceContractAddress { get; set; }
+
+        protected EvidenceContractTestBase()
+        {
+            InitializeContracts();
+        }
+
+        private void InitializeContracts()
+        {
+            ZeroContractStub = GetZeroContractStub(SampleECKeyPairs.KeyPairs.First());
+
+            EvidenceContractAddress = AsyncHelper.RunSync(() =>
+                ZeroContractStub.DeploySystemSmartContract.SendAsync(
+                    new SystemContractDeploymentInput
+                    {
+                        Category = KernelConstants.CodeCoverageRunnerCategory,
+                        Code = ByteString.CopyFrom(File.ReadAllBytes(typeof(EvidenceContract).Assembly.Location)),
+                        Name = ProfitSmartContractAddressNameProvider.Name,
+                        TransactionMethodCallList =
+                            new SystemContractDeploymentInput.Types.SystemTransactionMethodCallList()
+                    })).Output;
+            EvidenceContractStub = GetEvidenceContractStub(SampleECKeyPairs.KeyPairs.First());
+        }
+
+        private ACS0Container.ACS0Stub GetZeroContractStub(ECKeyPair keyPair)
+        {
+            return GetTester<ACS0Container.ACS0Stub>(ContractZeroAddress, keyPair);
+        }
+
+        private EvidenceContractContainer.EvidenceContractStub GetEvidenceContractStub(ECKeyPair keyPair)
+        {
+            return GetTester<EvidenceContractContainer.EvidenceContractStub>(EvidenceContractAddress, keyPair);
+        }
+    }
+}
---
+++ chain/test/AElf.Contracts.EvidenceContract.Test/EvidenceContractTestModule.cs
@@ -0,0 +1,15 @@
+﻿using AElf.Contracts.TestKit;
+using AElf.Kernel.SmartContract;
+using Volo.Abp.Modularity;
+
+namespace AElf.Contracts.EvidenceContract
+{
+    [DependsOn(typeof(ContractTestModule))]
+    public class EvidenceContractTestModule : ContractTestModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<ContractOptions>(o => o.ContractDeploymentAuthorityRequired = false);
+        }
+    }
+}
