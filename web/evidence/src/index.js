fd33ed226ef2409161b6c9eb0303dbe08a7dc09b
huangdongmei <849013525@qq.com>
2020-05-10 23:26:23 +0800

    fix some errors again

--- web/evidence/src/index.js
+++ web/evidence/src/index.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import { Input, Form, message, Button } from 'antd';
 import 'antd/dist/antd.css';
 import AElf from 'aelf-sdk';
-import uint from 'aelf-sdk/src/types/uint';
+
 
 const aelf = new AElf(new AElf.providers.HttpProvider('http://127.0.0.1:1235'));
 const priviteKeyWallet = AElf.wallet.getWalletByPrivateKey('b842c00d26be7a38cf049ec381df1841199ea15ec3cc460b074a56e1a2d480ae');
@@ -26,7 +26,7 @@ class App extends Component {
     super(props);
     this.state = {
       fileReceived : [],
-      fileName : '',
+      fileName : '图片6',
       hashCode : '',
     }
     this.inputFile = this.inputFile.bind(this);
@@ -36,44 +36,34 @@ class App extends Component {
   }
 
   //选择上传的文件
-  async inputFile(){
+  inputFile() {
     const fileReceived = document.querySelector('#input').files[0];
     const reader = new FileReader();
     reader.readAsArrayBuffer(fileReceived);
-
-    const fileBuffer = this.result;
-    const fileView = new DataView(fileBuffer);
-    const that = this;
-    reader.onload = function () {
-      that.setState({
-        fileReceived : fileView.getBigInt64(0),
-        fileName : fileReceived.name,},()=>that.handleSubmit());
+    reader.onload = async (event)=>{
+      const fileBuffer = event.target.result;
+      const hashCode = AElf.utils.sha256(fileBuffer);
+      const fileBytes = new Uint8Array(fileBuffer);
+      evidenceContract = await aelf.chain.contractAt(evidenceContractAddress, priviteKeyWallet);
+      (async () => {
+        let result = await evidenceContract.FilesToHash({
+          id: hashCode,
+          fileName: this.state.fileName,
+          fileByte: fileBytes,
+          fileSize: fileBytes.length,
+        });
+        console.log(result);
+      })();
     }
   }
 
   //提交选择的文件
-  async handleSubmit() {
-    const fileBytes = this.state.fileReceived;
-    const byteView = fileBytes.allBuffers;
-    const hashCode = AElf.utils.sha256(fileBytes);
-    evidenceContract = await aelf.chain.contractAt(evidenceContractAddress, priviteKeyWallet);
-
-    console.log("hashCode:"+hashCode);
+    async handleSubmit(inputFile) {
+/*    console.log("hashCode:"+hashCode);
     console.log("fileName:"+this.state.fileName);
     console.log("fileByte:"+fileBytes);
-    console.log("fileSize:"+fileBytes.byteLength);
-    console.log(evidenceContract);
-
-    (async () => {
-        let result = await evidenceContract.FilesToHash({
-        id: hashCode,
-        fileName: this.state.fileName,
-        fileByte: byteView,
-        fileSize: fileBytes.byteLength,
-      });
-      console.log(result);
-    })();
-
+    console.log("fileSize:"+fileBytes.length);
+    console.log(evidenceContract);*/
   }
 
   //取得输入的哈希码
