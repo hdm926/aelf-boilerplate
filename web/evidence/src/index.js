3d88536f5c8e5d330749bd433fcbbef8fdff4225
huangdongmei <849013525@qq.com>
2020-05-12 22:20:23 +0800

    ADD A PLANB

--- web/evidence/src/index.js
+++ web/evidence/src/index.js
@@ -30,12 +30,12 @@ class App extends Component {
       hashCode : '',
     }
     this.inputFile = this.inputFile.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.inputHashCode = this.handleHashCode.bind(this);
-    this.handleHashCode = this.handleHashCode.bind(this);
+    this.inputFilePlanB = this.inputFilePlanB.bind(this);
+    this.verifyHashCodePlanB = this.verifyHashCodePlanB.bind(this);
+    this.verifyHashCode = this.verifyHashCode.bind(this);
   }
 
-  //选择上传的文件
+  //选择并提交上传的文件
   inputFile() {
     const fileReceived = document.querySelector('#input').files[0];
     const reader = new FileReader();
@@ -52,36 +52,68 @@ class App extends Component {
           fileByte: fileBytes,
           fileSize: fileBytes.length,
         });
-        console.log(result);
+        document.getElementById("returnCode").innerHTML = hashCode;
+      })();
+    }
+  }
+  //只上传文件哈希码
+  inputFilePlanB(){
+    const fileReceived = document.querySelector('#input').files[0];
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(fileReceived);
+    reader.onload = async (event)=>{
+      const fileBuffer = event.target.result;
+      const hashCode = AElf.utils.sha256(fileBuffer);
+      const fileBytes = new Uint8Array(fileBuffer);
+      evidenceContract = await aelf.chain.contractAt(evidenceContractAddress, priviteKeyWallet);
+
+      (async () => {
+        let result = await evidenceContract.FilesToHashPlanB({
+          id: hashCode,
+          fileName: fileReceived.name,
+          fileSize: fileBytes.length,
+        });
+        document.getElementById("returnCode").innerHTML = hashCode;
       })();
     }
   }
 
-  //提交选择的文件
-    async handleSubmit(inputFile) {
-/*    console.log("hashCode:"+hashCode);
-    console.log("fileName:"+this.state.fileName);
-    console.log("fileByte:"+fileBytes);
-    console.log("fileSize:"+fileBytes.length);
-    console.log(evidenceContract);*/
+  //通过原文件和之前的哈希码验证
+   async verifyHashCodePlanB(){
+    const code = await document.getElementById("hashCode").value;//字符串类型
+    const fileReceived = document.querySelector('#input').files[0];
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(fileReceived);
+     reader.onload = async (event)=>{
+       const fileBuffer = event.target.result;
+       const hashCode = AElf.utils.sha256(fileBuffer);
+       evidenceContract = await aelf.chain.contractAt(evidenceContractAddress, priviteKeyWallet);
+       (async () => {
+         let result = await evidenceContract.VerifyFilesPlanB({
+           hashInput: code,
+           hashFromFile: hashCode,
+         });
+         alert(result);
+       })();
+
+     }
   }
 
-  //取得输入的哈希码
-   async handleHashCode(){
-    const code = await document.getElementById("hashCode").value;
-    this.setState({
-      hashCode: code,
-    });
+  //通过哈希码验证，并返回原文件
+  async verifyHashCode(){
+    const code =  document.getElementById("hashCode").value;
+    evidenceContract = await aelf.chain.contractAt(evidenceContractAddress, priviteKeyWallet);
 
-    console.log("state:"+this.state.hashCode);
-    evidenceContract =  await aelf.chain.contractAt(evidenceContractAddress, priviteKeyWallet);
-    console.log(evidenceContract);
+      (async () => {
+      let result = await evidenceContract.VerifyFiles.call({
+          hashInput: code,
+        });
+        console.log(result);
+      })();
 
-    (async () => {
-      let result = await evidenceContract.VerifyFiles.call(this.state.hashCode);
-      console.log(result);
-    })();
-  }
+     /* var fileBytes = new ArrayBuffer(result.value);
+      var blob = new Blob([fileBytes]);*/
+    }
 
   render() {
      if(!aelf.isConnected()) {
@@ -95,21 +127,31 @@ class App extends Component {
 
           <div id="input_div">
             选择文件
-            <Input type="file" id="input" onChange={this.inputFile}/>
+            <Input type="file" id="input"/>
           </div>
 
           <div>
-            <button onClick={()=>this.handleSubmit()}>提交</button>
+          <button onClick={()=>this.inputFile()}>提交图片信息</button>
           </div>
+
+          <div>
+            <button onClick={()=>this.inputFilePlanB()}>提交哈希值</button>
+          </div>
+
+          <p id="returnCode">文件id</p>
         </Form>
 
           <div id = 'verify'>
-        <Input type = "textarea" id = "hashCode"/>
+        <Input type = "text" id = "hashCode"/>
       </div>
 
           <div>
-            <button onClick={()=>this.handleHashCode()}>验证</button>
+            <button onClick={()=>this.verifyHashCodePlanB()}>验证图片和哈希码</button>
           </div>
+
+        <div>
+          <button onClick={()=>this.verifyHashCode()}>验证哈希码</button>
+        </div>
 
 
       </div>
